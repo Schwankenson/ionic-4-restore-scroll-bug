f2c62e662ee52dc68c97016d8e9d45e681b4bf90
JS <johannes.schwenk@posteo.de>
2019-05-09 18:51:09 +0200

    Add example data to list

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { exampleData } from '../example-data'
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+	data = exampleData
 
-  constructor() { }
+	constructor() { }	
 
-  ngOnInit() {
+  ngOnInit() {  	
+
   }
 
 }
