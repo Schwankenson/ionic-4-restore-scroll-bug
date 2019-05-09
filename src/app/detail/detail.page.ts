02e66e2f953bb21ce350638b54bed2c20ccd5f2d
JS <johannes.schwenk@posteo.de>
2019-05-09 20:11:55 +0200

    Add detail page

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { exampleData } from '../example-data'
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
+	private curItem;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+  	let curItemId = this.activatedRoute.snapshot.params.id;
+
+  	exampleData.forEach(item => {
+  		if (item.id.toString() === curItemId) {
+  			this.curItem = item;
+  		}
+  	});  	
   }
 
 }
